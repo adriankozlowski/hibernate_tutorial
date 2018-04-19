18f66774ff4a4ae608e89edddfa6fb962388f0c9
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-04-19 16:57:42 +0200

    sample excercise

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -1,34 +1,75 @@
 package pl.sda;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import pl.sda.model.Customer;
-import pl.sda.model.Reservation;
-
-import java.util.HashSet;
+import org.hibernate.cfg.Configuration;
 
 
 public class Main {
-    public static void main(String[] args) {
-        SessionFactory instance = HbnConfig.getInstance();
-        Session session = instance.openSession();
-        Transaction transaction = session.beginTransaction();
-        Reservation reservation = new Reservation();
-        if (reservation.getCustomers() == null) {
-            reservation.setCustomers(new HashSet<>());
-        }
+    private static SessionFactory factory;
 
-        Customer customer = new Customer();
-        session.save(customer);
-        if (customer.getReservation() == null) {
-            customer.setReservation(new HashSet<>());
+    public static void main(String[] args) {
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
         }
-        customer.getReservation().add(reservation);
-        reservation.getCustomers().add(customer);
-        session.save(reservation); //wtf?!
-        transaction.commit();
-        session.close();
-        instance.close();
+        Main manage = new Main();
+
+        //Dodaj kilku pracowników do bazy danych
+        Integer empID1 = manage.addEmployee("Zara", "Ali", 1000);
+        Integer empID2 = manage.addEmployee("Daisy", "Das", 5000);
+        Integer empID3 = manage.addEmployee("John", "Paul", 10000);
+
+        //wypisz wszystkich pracowników
+        manage.listEmployees();
+
+        //zaktualizuj jeden rekord
+        manage.updateEmployee(empID1, 5000);
+
+        //usuñ pracownika z bazy danych
+        manage.deleteEmployee(empID2);
+
+        //wypisz wysztkich pracowników po zmianach
+        manage.listEmployees();
+    }
+
+    public Integer addEmployee(String fname, String lname, int salary) {
+        //utwórz sesję
+        //utwórz transakcję
+        //rozpocznij transakcję
+        //utwórz nowy obiekt typu Employee
+        //zapisz obiekt typu Employee, przechowuj jego id
+        //zrób commit transakcji
+        Integer employeeID = null;
+
+        return employeeID;
+    }
+
+    public void listEmployees() {
+        //utwórz sesję
+        //utwórz transakcję
+        //rozpocznij transakcję
+        //wykonaj zapytanie u¿ywaj¹c createQuery
+        //wypisz do konsoli wszystkich u¿ytkowników
+        //zakoñcz transakcję
+    }
+
+    public void updateEmployee(Integer EmployeeID, int salary) {
+        //utwórz sesję
+        //utwórz transakcję
+        //rozpocznij transakcję
+        //pobierz z sesji u¿ytkownika po podanym id
+        //ustaw u¿ytkownikowi now¹ wartoœæ wynagrodzenia
+        //zrób commit transakcji
+    }
+
+    public void deleteEmployee(Integer EmployeeID) {
+        //utwórz sesję
+        //utwórz transakcję
+        //rozpocznij transakcję
+        //pobierz z sesji u¿ytkownika po podanym id
+        //usuñ u¿ytkownika
+        //zrób commit transakcji
     }
 }
